6fcee67ea68a8ec248316b8caa9448a811054fa6
putrirevalinasitumeang <putri.revalina@student.undiksha.ac.id>
2026-07-01 11:10:35 +0800

    Add files via upload

--- config/DBConnection.java
+++ config/DBConnection.java
@@ -1,1 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+
+        try {
+            if (connection == null || connection.isClosed()) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/javadb",
+                        "root",
+                        "250907"
+                );
+            System.out.println("Database berhasil terkoneksi.");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
